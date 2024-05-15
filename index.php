2c3f6dfb35a497c679a1f6e464bcb4119c9efa28
Wajahat Abbas <155806602+DevWajahat@users.noreply.github.com>
2024-05-15 20:49:33 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    // conditional operator
+    // == , !=
+
+// if (2==2){
+    //     echo "good";
+    // }
+    // else{
+    //     echo "bad";
+    // }
+
+
+    // logical operator
+    // if((10==12) || (20==20)){
+    //     echo "good";
+    // }
+    // else {
+    //     echo "bad";
+    // }
+    // if((10==12) && (20==20)){
+    //     echo "good";
+    // }
+    // else {
+    //     echo "bad";
+    // }
+    
+    // class task
+
+//     $a = 40;
+//    if($a > 100) {
+//     echo "var is greater than 100";
+//    } 
+//    else if ($a == 100){
+//     echo "var is equal to 100";
+//    }
+//    else {
+//     echo "var is less than 100";
+//    }
+
+// switch case 
+
+// $t = 20;
+// switch ($t)
+// {
+//     case $t>100:
+//         echo "var is greater than 100";
+//         break;
+
+//         case $t==100:
+//             echo "var is equal to 100";
+//             break;
+
+//             default:
+//             echo "var is less than 100";
+// }
+    ?>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,11 @@
+Operators 
+Logical operators
+
+OR operator NOT operator
+
+
+
+Conditional Operators
+
+
+switch statement
